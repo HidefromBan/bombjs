3fe31711935320c9e7eed61553da81b0f8a2ed31
Josh <hidefromban@gmail.com>
2023-10-01 23:43:54 -0300

    Obj playerPosition, movimiento de jugador

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
-
-let canvasSize;
-let elementSize;
 const canvas = document.getElementById('game');
 const game = canvas.getContext('2d');
 const btnUp = document.querySelector('#up')
 const btnDown = document.querySelector('#down')
 const btnRight = document.querySelector('#right')
 const btnLeft = document.querySelector('#left')
+
+let canvasSize;
+let elementSize;
+
+const playerPosition = {
+    x: undefined,
+    y: undefined,
+}
+
 
 window.addEventListener('load',setCanvasSize);
 window.addEventListener('resize',setCanvasSize);
@@ -42,12 +48,19 @@ function startGame(){
             const emoji = emojis[col];
             const posX = elementSize * (colI );
             const posY = elementSize * (rowI + .9);
-
+            if(col == 'O'){
+                playerPosition.x = posX;
+                playerPosition.y = posY;
+                console.log({playerPosition})
+            }
             game.fillText(emoji,posX,posY)
-
         })
     });
-        
+    movePlayer();
+}
+
+function movePlayer(){
+    game.fillText(emojis['PLAYER'],playerPosition.x,playerPosition.y);
 }
 
 window.addEventListener('keydown',moveKeys);
@@ -76,18 +89,23 @@ function moveKeys(e){
 
 function moveUp(){
     console.log('MoveUp');
+    playerPosition.y -= elementSize;
+    movePlayer();
 }
 function moveDown(){
     console.log('MoveDown');
-
+    playerPosition.y += elementSize;
+    movePlayer();
 }
 function moveLeft(){
     console.log('MoveLeft');
-
+    playerPosition.x -= elementSize;
+    movePlayer();
 }
 function moveRight(){
     console.log('MoveRight');
-
+    playerPosition.x += elementSize;
+    movePlayer();
 }
 
 
